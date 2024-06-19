74000e004adcbb4270afc5205d6c2fcc2d18ba7a
oussemakessentini <oussamaksantini@gmail.com>
2024-06-19 00:08:54 -0500

    fix services

--- src/main/java/com/example/ecommerce/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/ecommerce/service/impl/ProductServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.ecommerce.service.impl;
 import com.example.ecommerce.entity.dto.request.ProductRequest;
 import com.example.ecommerce.entity.dto.response.ProductResponse;
 import com.example.ecommerce.entity.product.Product;
+import com.example.ecommerce.entity.product.Review;
 import com.example.ecommerce.repository.ProductRepository;
 import com.example.ecommerce.service.ProductService;
 import com.example.ecommerce.service.generic.GenericServiceImpl;
@@ -10,6 +11,8 @@ import com.example.ecommerce.util.ListMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductServiceImpl extends GenericServiceImpl<Product, ProductRequest, ProductResponse, Long> implements ProductService {
@@ -24,5 +27,14 @@ public class ProductServiceImpl extends GenericServiceImpl<Product, ProductReque
     @Autowired
     public ProductServiceImpl(ProductRepository productRepository, ModelMapper modelMapper, ListMapper listMapper){
         super(productRepository, modelMapper, listMapper);
+    }
+
+    @Override
+    public List<Review> getProductReviews(Long id) {
+        Product product = productRepository.findById(id).orElse(null);
+        if (product != null) {
+            return product.getReviews();
+        }
+        return List.of();
     }
 }
